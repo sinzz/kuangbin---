6cb937da8a1dda1dca1f13e2af950c1ac451406f
sinzz <292535961@qq.com>
2015-10-14 21:06:30 +0800

    +
    +poj 1611

--- 并查集/The Suspects/The Suspects/1.cpp
+++ 并查集/The Suspects/The Suspects/1.cpp
@@ -1,56 +1,88 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define N 3030
-int num, g, m;
+#define N 40000
 int pre[N];
-int vis[N];
+int cnt[N];
+int rank[N] = { 0 };
+void makeSet(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		pre[i] = i;
+		cnt[i] = 1;
+	}
+}
+int find2(int x)
+{
+	if (pre[x] == x) return x;
+	int rec[N], root, k = 0;
+	for (; pre[x] != x; x = pre[x])
+		rec[k++] = x;
+	root = x;
+	for (x = 0; x != k; x++)
+		pre[rec[x]] = root;
+	return root;
+}
 int find(int x)
 {
-	int r = x;
-	while (pre[r] != r)
-		r = pre[r];
-	return r;
+	if (pre[x] != x)
+		pre[x] = find(pre[x]);
+	return pre[x];
 }
-void join(int x, int y)
+void join(int x, int y)//³¬Ê±
+{
+	int fx = find2(x);
+	int fy = find2(y);
+	if (fx != fy)
+	{
+		pre[fx] = fy;
+		cnt[fy] += cnt[fx];
+	}
+
+}
+bool join2(int x, int y)
 {
 	int fx = find(x);
 	int fy = find(y);
-	if (fx != fy)
+	if (fx == fy)return false;
+	if (rank[fx] > rank[fy])
 	{
-		pre[x] = y;
+		pre[fy] = fx;
+		cnt[fx] += cnt[fy];
 	}
-	else if (fx == 0)
-		pre[y] = 0;
+	else{
+		pre[fx] = fy;
+		cnt[fy] += cnt[fx];
+		if (rank[fx] == rank[fy])
+			rank[fy]++;
+
+	}
+		
+	
+	return true;
 }
 int main(void)
 {
-	int k,f;
-	int i = 0;
-	int sum=1;
-	scanf("%d%d", &num, &g);
-	while (num != 0 && g != 0)
+	int n, m;
+	while (scanf("%d%d", &n, &m)  && (n || m))
 	{
-		memset(pre, -1, sizeof(pre));
-		memset(vis, 0, sizeof(vis));
-		pre[0] = 0;
-		while (g--)
+		makeSet(n);
+		for (int i = 0; i != m; i++)
 		{
-			scanf("%d%d", &m, &k);
-			pre[k] = k;
-			m--;
-			while (m--)
+			int t, f, c;
+			scanf("%d%d", &t, &f);
+			for (int j = 1; j != t; j++)
 			{
-				scanf("%d", &f);
-				join(f, k);
+				scanf("%d", &c);
+// 				if (c == find(c))
+// 
+// 					join(c, f);
+// 				else
+// 					join(f, c);
+				join2(f, c);
 			}
-			
 		}
-		while (i < num)
-		{
-			if (pre[i] != -1 && find(i) == 0)
-				sum++;
-
-		}
+		printf("%d\n", cnt[find(0)]);
 	}
 }
